e3c20b368d837ca243b85104b67789625e892256
irendon1993 <isaacrendon7@gmail.com>
2022-05-10 15:45:34 -0400

    Setting up env url

--- src/app/service/pokemon.service.ts
+++ src/app/service/pokemon.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs'
 import { Pokemon } from '../interface/pokemon';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getPokemons(): Observable<Pokemon[]> {
-    return this.http.get<Pokemon[]>('http://127.0.0.1:3000/pokemon/')
+    return this.http.get<Pokemon[]>(`${this.apiUrl}/pokemon`)
   }
 
   getPokemon(): Observable<Pokemon> {
-    return this.http.get<Pokemon>('http://127.0.0.1:3000/pokemon/237')
+    return this.http.get<Pokemon>(`${this.apiUrl}/pokemon/245`)
   }
 }
