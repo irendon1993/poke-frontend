ba7df2eb179eb9d15a875bed06579bc09796eaa8
irendon1993 <isaacrendon7@gmail.com>
2022-06-10 14:36:48 -0400

    Fixed init bug with second welcome screen

--- src/app/game-ui/game-ui.component.ts
+++ src/app/game-ui/game-ui.component.ts
@@ -11,22 +11,51 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 
 export class GameUiComponent implements OnInit {
+  newGameResponse: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  enterZoneResponse: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  newTrainerResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  
+  gameInitResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  
+  zoneGetResponse: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  zoneSetResponse: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  directionsResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  directions:BehaviorSubject<any> = new BehaviorSubject<any>([]);
+  
+  getTrainerResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  trainerResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  
+  catchPokemonResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  zoneResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  wildPokemonResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  wildPokemon:BehaviorSubject<any> = new BehaviorSubject<any>([]);
+  randomWildPokemon = 0;
+  pokemonToCatch:BehaviorSubject<any> = new BehaviorSubject<any>([]);
 
-  gameResponse: BehaviorSubject<any> = new BehaviorSubject<any>({});
-  pokeResponse: BehaviorSubject<any> = new BehaviorSubject<any>({});
+
+  throwPokeballResponse: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  pokeballThrownResponse: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  pokemonToPcResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  pcResponse: BehaviorSubject<any> = new BehaviorSubject([]);
+  pc: any;
+  pics: any;
+  pcPicArray: any;
+
+  optionOneResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  optionOnePressedResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  optionOneDirectionsResponse: BehaviorSubject<any> = new BehaviorSubject({});
+  nextZoneResponse: BehaviorSubject<any> = new BehaviorSubject({});
+
+
+  // pokeResponse: BehaviorSubject<any> = new BehaviorSubject<any>({});
   party:BehaviorSubject<any> = new BehaviorSubject<any>([]);
 
-  zoneResponse: BehaviorSubject<any> = new BehaviorSubject({});
   zone: BehaviorSubject<any> = new BehaviorSubject([]);
 
-  nextZoneResponse: BehaviorSubject<any> = new BehaviorSubject({});
   nextZone: BehaviorSubject<any> = new BehaviorSubject([]);
 
   
   
-  trainerResponse: BehaviorSubject<any> = new BehaviorSubject({});
-  directionsResponse: BehaviorSubject<any> = new BehaviorSubject({});
-  directions:BehaviorSubject<any> = new BehaviorSubject<any>([]);
   
   picArray:BehaviorSubject<any> = new BehaviorSubject<any>([]);
   
@@ -34,23 +63,13 @@ export class GameUiComponent implements OnInit {
   traveling = false;
   catching = false;
   newGame = true;
+  inCenter = false;
   gameOver = false;
   pokeBalls= 0;
   
-  newTrainerResponse: BehaviorSubject<any> = new BehaviorSubject({});
-  
-
-  wildPokemonResponse: BehaviorSubject<any> = new BehaviorSubject({});
-  wildPokemon:BehaviorSubject<any> = new BehaviorSubject<any>([]);
-  randomWildPokemon = 0;
-  pokemonToCatch:BehaviorSubject<any> = new BehaviorSubject<any>([]);
   
   
-  pokemonToPcResponse: BehaviorSubject<any> = new BehaviorSubject({});
-  pcResponse: BehaviorSubject<any> = new BehaviorSubject([]);
-  pc: any;
-  pics: any;
-  pcPicArray: any;
+  
  
   // pc:BehaviorSubject<any> = new BehaviorSubject<any>([]);
   
@@ -82,15 +101,30 @@ export class GameUiComponent implements OnInit {
     this.gameService.updateActiveTrainer(0).subscribe()
     window.location.reload()
   }
-  
-  onNewGame() {
-    this.gameService.newGame(this.trainerName).subscribe(
+
+  enterZone() {
+    this.gameService.getLastTrainer().subscribe(
       (response) => {
-        this.pokeResponse.next(response);
+        this.enterZoneResponse.next(response);
       },
       (error: any) => console.log(error),
       () => {
         this.gameService.setGameState(1).subscribe()
+        this.gameService.addPokemonToPc(this.enterZoneResponse.value.id,[]).subscribe()
+        this.gameService.addPcPic(this.enterZoneResponse.value.id,[]).subscribe()
+        window.location.reload() 
+      }
+    )
+  }
+  
+  onNewGame() {
+    this.gameService.newGame(this.trainerName).subscribe(
+      (response) => {
+        this.newGameResponse.next(response);
+      },
+      (error: any) => console.log(error),
+      () => {
+      
         this.gameService.getLastTrainer().subscribe(
           (response) => {
             this.newTrainerResponse.next(response);
@@ -98,10 +132,13 @@ export class GameUiComponent implements OnInit {
           (error: any) => console.log(error),
           () => {
             // console.log(this.newTrainerResponse.value.id)
+            
             this.gameService.updateActiveTrainer(this.newTrainerResponse.value.id).subscribe()
             this.gameService.changeZoneState(this.newTrainerResponse.value.id,4).subscribe()
             this.gameService.setPokeballs(this.newTrainerResponse.value.id,3).subscribe()
-            window.location.reload()
+            this.newGame = false;
+            this.inCenter = true;
+            
             
           }
         )
@@ -113,37 +150,36 @@ export class GameUiComponent implements OnInit {
   onGameInit() {
     this.gameService.getGameState().subscribe(
       (response) => {
-        this.pokeResponse.next(response);
+        this.gameInitResponse.next(response);
       },
       (error: any) => console.log(error),
       () => {
-            this.newGame = this.pokeResponse.value.game_state;
-            if(this.pokeResponse.value.game_state == 1) {
+            this.newGame = this.gameInitResponse.value.game_state;
+            if(this.gameInitResponse.value.game_state == 1) {
               this.newGame = false;
               this.traveling = true;
             }
             
-            this.gameService.addPokemonToPc(this.pokeResponse.value.active_trainer,[]).subscribe()
-            this.gameService.addPcPic(this.pokeResponse.value.active_trainer,[]).subscribe()
-            console.log(this.pokeResponse.value)
+            
+            console.log(this.gameInitResponse.value)
            }
     )
   }
   onGetZone() {
     this.gameService.getGameState().subscribe(
       (response) => {
-        this.gameResponse.next(response);
+        this.zoneGetResponse.next(response);
       },
       (error: any) => console.log(error),
       () => {
-      this.gameService.getTrainer(this.gameResponse.value.active_trainer).subscribe(
+      this.gameService.getTrainer(this.zoneGetResponse.value.active_trainer).subscribe(
       
       (response) => {
-        this.pokeResponse.next(response);
+        this.zoneSetResponse.next(response);
       },
       (error: any) => console.log(error),
       () => {
-        this.zone.next(JSON.parse(this.pokeResponse.value.currentZone))
+        this.zone.next(JSON.parse(this.zoneSetResponse.value.currentZone))
         this.gameService.getZoneData(this.zone.value).subscribe(
           
           (response) => { 
@@ -165,11 +201,11 @@ export class GameUiComponent implements OnInit {
 onGetTrainerId() {
   this.gameService.getGameState().subscribe(
     (response) => {
-      this.pokeResponse.next(response);
+      this.getTrainerResponse.next(response);
     },
     (error: any) => console.log(error),
     () => {
-    this.gameService.getTrainer(this.pokeResponse.value.active_trainer).subscribe(
+    this.gameService.getTrainer(this.getTrainerResponse.value.active_trainer).subscribe(
     (response) => {
       this.trainerResponse.next(response);
     },
@@ -203,18 +239,18 @@ onGetTrainerId() {
 
     this.gameService.getGameState().subscribe(
       (response) => {
-        this.pokeResponse.next(response);
+        this.catchPokemonResponse.next(response);
       },
       (error: any) => console.log(error),
       () => {
-      this.gameService.getTrainer(this.pokeResponse.value.active_trainer).subscribe(
+      this.gameService.getTrainer(this.catchPokemonResponse.value.active_trainer).subscribe(
 
       (response) => {
-        this.pokeResponse.next(response);
+        this.catchPokemonResponse.next(response);
       },
       (error: any) => console.log(error),
       () => {
-        this.zone.next(JSON.parse(this.pokeResponse.value.currentZone))
+        this.zone.next(JSON.parse(this.catchPokemonResponse.value.currentZone))
         this.gameService.getZoneData(this.zone.value).subscribe(
           
           (response) => { 
@@ -236,7 +272,7 @@ onGetTrainerId() {
               (error: any) => console.log(error),
               () => {
                 this.pokemonToCatch.next(this.wildPokemonResponse.value.iamgeurl)
-                this.gameService.changeCurrentPokemon(this.pokeResponse.value.id, this.wildPokemonResponse.value.id).subscribe()
+                this.gameService.changeCurrentPokemon(this.catchPokemonResponse.value.id, this.wildPokemonResponse.value.id).subscribe()
                 
               }
             )
@@ -251,38 +287,38 @@ onGetTrainerId() {
   throwPokeball() {
     this.gameService.getGameState().subscribe(
       (response) => {
-        this.gameResponse.next(response);
+        this.throwPokeballResponse.next(response);
       },
       (error: any) => console.log(error),
       () => {
       // console.log(this.gameResponse.value)
-      this.gameService.getTrainer(this.gameResponse.value.active_trainer).subscribe(
+      this.gameService.getTrainer(this.throwPokeballResponse.value.active_trainer).subscribe(
       (response) => {
-        this.pokeResponse.next(response);
+        this.pokeballThrownResponse.next(response);
       },
       (error: any) => console.log(error),
       () => {
         
-        this.pokeBalls = this.pokeResponse.value.pokeballs
+        this.pokeBalls = this.pokeballThrownResponse.value.pokeballs
         // this.pokeBalls = this.pokeBalls.toInt();
-        console.log(this.pokeResponse.value)
+        console.log(this.pokeballThrownResponse.value)
         console.log(this.pokeBalls)
-        this.gameService.setPokeballs(this.pokeResponse.value.id,this.pokeBalls-1).subscribe()
+        this.gameService.setPokeballs(this.pokeballThrownResponse.value.id,this.pokeBalls-1).subscribe()
         console.log(this.pokeBalls)
         if(this.pokeBalls <= 0) {
-          this.pics = JSON.parse(this.pokeResponse.value.pic_array);
+          this.pics = JSON.parse(this.pokeballThrownResponse.value.pic_array);
             this.gameOver = true;
             console.log(this.pics)
           }
 
         else {
-          console.log(this.pokeResponse.value)
-          this.pc = JSON.parse(this.pokeResponse.value.pc)
+          console.log(this.pokeballThrownResponse.value)
+          this.pc = JSON.parse(this.pokeballThrownResponse.value.pc)
           // console.log(this.pc)
-          this.pc.push(this.pokeResponse.value.current_pokemon)
+          this.pc.push(this.pokeballThrownResponse.value.current_pokemon)
           
           
-          this.gameService.getPokemon(this.pokeResponse.value.current_pokemon).subscribe(
+          this.gameService.getPokemon(this.pokeballThrownResponse.value.current_pokemon).subscribe(
             (response) => {
                     this.pokemonToPcResponse.next(response);
                   },
@@ -291,20 +327,20 @@ onGetTrainerId() {
 
                     // console.log(this.pokemonToPcResponse.value.iamgeurl)
 
-                    this.pics = JSON.parse(this.pokeResponse.value.pic_array);
+                    this.pics = JSON.parse(this.pokeballThrownResponse.value.pic_array);
                     // this.pics.push(this.pokeResponse.value.current_pokemon)
                     this.pics.push(this.pokemonToPcResponse.value.iamgeurl)
 
                     if(this.pics[0] == 0) {
                       this.pics.shift();
                       // console.log(this.pics)
-                      this.gameService.addPcPic(this.pokeResponse.value.id, this.pics).subscribe()
-                      this.gameService.addPokemonToPc(this.pokeResponse.value.id, this.pc).subscribe()
+                      this.gameService.addPcPic(this.pokeballThrownResponse.value.id, this.pics).subscribe()
+                      this.gameService.addPokemonToPc(this.pokeballThrownResponse.value.id, this.pc).subscribe()
                     } else {
 
                     // console.log(this.pics)
-                    this.gameService.addPcPic(this.pokeResponse.value.id, this.pics).subscribe()
-                    this.gameService.addPokemonToPc(this.pokeResponse.value.id, this.pc).subscribe()
+                    this.gameService.addPcPic(this.pokeballThrownResponse.value.id, this.pics).subscribe()
+                    this.gameService.addPokemonToPc(this.pokeballThrownResponse.value.id, this.pc).subscribe()
                     }
                     
                   }
@@ -331,55 +367,127 @@ onGetTrainerId() {
     // On submit update trainer zone
     this.gameService.getGameState().subscribe(
       (response) => {
-        this.gameResponse.next(response);
+        this.optionOneResponse.next(response);
       },
       (error: any) => console.log(error),
       () => {
-      this.gameService.getTrainer(this.gameResponse.value.active_trainer).subscribe(
+      this.gameService.getTrainer(this.optionOneResponse.value.active_trainer).subscribe(
 
       (response) => {
-        this.pokeResponse.next(response);
+        this.optionOnePressedResponse.next(response);
       },
       (error: any) => console.log(error),
       () => {
-        this.zone.next(JSON.parse(this.pokeResponse.value.currentZone))
+        this.zone.next(JSON.parse(this.optionOnePressedResponse.value.currentZone))
         this.gameService.getZoneData(this.zone.value).subscribe(
           
           (response) => { 
-            this.directionsResponse.next(response);
+            this.optionOneDirectionsResponse.next(response);
           },
           (error: any) => console.log(error),
           () => {
-            this.nextZone.next(JSON.parse(this.directionsResponse.value.next_zone))
-            this.gameService.changeZoneState(this.pokeResponse.value.id, this.nextZone.value[1]).subscribe(
+            this.nextZone.next(JSON.parse(this.optionOneDirectionsResponse.value.next_zone))
+            this.gameService.changeZoneState(this.optionOnePressedResponse.value.id, this.nextZone.value[0]).subscribe(
             
             (response) => { 
                 this.nextZoneResponse.next(response);
-                // console.log(this.nextZoneResponse)
             },
             (error: any) => console.log(error),
             () => {
-              console.log(this.nextZone)
-              console.log(this.pokeResponse.value.id)
-              console.log(this.nextZone.value[0])
-            
-
               window.location.reload()
-              // this.gameState()
-              // this.gameService.getZoneData(this.nextZone.value[0])  
             }
-
-            
             );
-            
-            }
-            
+            } 
         ) 
       },
-
-
     )
   }
     )
+}
+
+
+onOptionTwo(): void {
+  // On submit update trainer zone
+  this.gameService.getGameState().subscribe(
+    (response) => {
+      this.optionOneResponse.next(response);
+    },
+    (error: any) => console.log(error),
+    () => {
+    this.gameService.getTrainer(this.optionOneResponse.value.active_trainer).subscribe(
+
+    (response) => {
+      this.optionOnePressedResponse.next(response);
+    },
+    (error: any) => console.log(error),
+    () => {
+      this.zone.next(JSON.parse(this.optionOnePressedResponse.value.currentZone))
+      this.gameService.getZoneData(this.zone.value).subscribe(
+        
+        (response) => { 
+          this.optionOneDirectionsResponse.next(response);
+        },
+        (error: any) => console.log(error),
+        () => {
+          this.nextZone.next(JSON.parse(this.optionOneDirectionsResponse.value.next_zone))
+          this.gameService.changeZoneState(this.optionOnePressedResponse.value.id, this.nextZone.value[1]).subscribe(
+          
+          (response) => { 
+              this.nextZoneResponse.next(response);
+          },
+          (error: any) => console.log(error),
+          () => {
+            window.location.reload()
+          }
+          );
+          } 
+      ) 
+    },
+  )
+}
+  )
+}
+
+
+onOptionThree(): void {
+  // On submit update trainer zone
+  this.gameService.getGameState().subscribe(
+    (response) => {
+      this.optionOneResponse.next(response);
+    },
+    (error: any) => console.log(error),
+    () => {
+    this.gameService.getTrainer(this.optionOneResponse.value.active_trainer).subscribe(
+
+    (response) => {
+      this.optionOnePressedResponse.next(response);
+    },
+    (error: any) => console.log(error),
+    () => {
+      this.zone.next(JSON.parse(this.optionOnePressedResponse.value.currentZone))
+      this.gameService.getZoneData(this.zone.value).subscribe(
+        
+        (response) => { 
+          this.optionOneDirectionsResponse.next(response);
+        },
+        (error: any) => console.log(error),
+        () => {
+          this.nextZone.next(JSON.parse(this.optionOneDirectionsResponse.value.next_zone))
+          this.gameService.changeZoneState(this.optionOnePressedResponse.value.id, this.nextZone.value[2]).subscribe(
+          
+          (response) => { 
+              this.nextZoneResponse.next(response);
+          },
+          (error: any) => console.log(error),
+          () => {
+            window.location.reload()
+          }
+          );
+          } 
+      ) 
+    },
+  )
+}
+  )
 }
 }
