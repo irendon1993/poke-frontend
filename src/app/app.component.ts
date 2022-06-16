d2142bd4c827bc9c06a74c120fce3964a4647ec7
irendon1993 <isaacrendon7@gmail.com>
2022-06-16 15:35:22 -0400

    Updated frontend to not parse new database fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,64 +28,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     
-    // this.onGetUserId();
-    // this.onGetTrainerId();
-    // this.onGetUserData();
-    // this.onGetPokemons();
-    // this.onGetPokemon();
-    // this.onCreatePokemon();
-    }
-
-    // onGetUserId() {
-    //   this.pokemonService.getUserId()
-    //   .subscribe(data => this.user = {
-    //     id: (data as any).id,
-    //     name:  (data as any).name,
-    //   });
-    // }
-
-  // onGetTrainerId(): void {
-  //   this.pokemonService.getUserId().subscribe(
-  //     (response) => { 
-  //     const test = response.pokeParty
-  //     console.log(test)
-  //     console.log(response)
-  //     this.pokeResponse.next(response);
-  //   },
-  //     (error: any) => console.log(error),
-  //     () => {
-  //     this.pokeParty.next(JSON.parse(this.pokeResponse.value.poke_party)) 
-  //     // console.log(this.pokeParty.value)
-  //     console.log(this.pokeParty.value[0])
-  //     } 
-  //   );
-  // }
-
-  // onGetPokemons(): void {
-  //   this.pokemonService.getPokemons().subscribe(
-  //     (response) => console.table(response),
-  //     (error: any) => console.log(error),
-  //     () => console.log('Done getting Poekmon')
-  //   );
-  // }
-
-  // onGetPokemon(): void {
-  //   this.pokemonService.getPokemon().subscribe(
-  //     (response) => console.table(response),
-  //     (error: any) => console.log(error),
-  //     () => console.log('Done getting Poekmon')
-  //   );
-  // }
-
-  // onCreatePokemon(): void {
-  //   this.pokemonService.createPokemon(this.pokemon).subscribe(
-  //     (response) => console.log(response),
-  //     (error: any) => console.log(error),
-  //     () => console.log('Done creating Pokemon')
-  //   );
-  // }
-
-
+   
    
 
   
